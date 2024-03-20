cdb6ec2966a81af8d053aea59829c0bee7a232f8
Tatev Kamavosyan <kamavosyantatev@gmail.com>
2024-03-20 13:46:05 +0400

    project one

---
+++ main.py
@@ -0,0 +1,35 @@
+var1=input('Enter a number:  ')
+var2=input('Enter measure of time:  ')
+var3=input('Mode of transportation:  ')
+var4=input('Enter an adjective:  ')
+var5=input('Enter an adjective2:  ')
+var6=input('Enter none:  ')
+var7=input('Enter a color:  ')
+var8=input('Enter part of the Body:  ')
+var9=input('Enter a verb:  ')
+var10=input('Enter a number2:  ')
+var11=input('Enter none2:  ')
+var12=input('Enter none3:  ')
+var13=input('Enter part of the Body2: ') 
+var14=input('Enter a verb:  ')
+var15=input('Enter none4:  ')
+var16=input('Enter an adjective3:  ')
+var17=input('Enter a silly word:  ')
+var18=input('Enter none:  ')
+
+
+
+
+    
+print(f"It was about {var1} {var2} ago when I arrived at the hospital in a {var3}. The hospital is a/an {var4} place, there are a lot of {var5} {var6} here. There are nurseshere who have {var7} {var8}. f someone wants to come into my room I toldthem that they have to {var9} first. I’ve decorated my room with {var10} {var11}. Today I talked to a doctor and they were wearing a {var12} on their {var13}. I heard that all doctors {var14} {var15}very day for breakfast. The most {var16} thing about being in the hospital is the {var17} {var18}!")
+
+
+
+
+
+
+
+
+ 
+ 
+
